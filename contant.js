80e5c43e1bdc73f51ba784169dc9a7faa90bfa5f
Angel Brand <AngelBrand1>
2023-06-21 21:52:32 -0500

    contant

---
+++ contant.js
@@ -0,0 +1,19 @@
+
+
+const classConceptInfo = {
+
+    'Database administrator' : {
+        name: 'Database administrator',
+        container: 'databaseAdministrator',
+        attributes: ['name', 'id'],
+        attributesNames: ['Name', 'ID'],
+    },
+
+    'User' : {
+        name: 'User',
+        container: 'user',
+        attributes: ['name', 'mail', 'username', 'id'],
+        attributesNames: ['Name', 'Mail', 'Username',  'ID'],
+    },
+
+} 
